375b91d6c5a8f994e8c0dd94f3f138904d80df3e
Thassio <thxssio@gmail.com>
2025-03-07 20:32:37 -0300

    fix: find package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="V3SProtoComm",
-    version="1.0.0",
+    version="2.0.0",
     description="Pacote para comunicação e controle de robôs via Protobuf",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
@@ -18,23 +18,9 @@ setup(
         "toml",
         "wrapt",
     ],
-    extras_require={
-        "dev": [
-            "astroid",
-            "isort",
-            "lazy-object-proxy",
-            "mccabe",
-            "pylint",
-        ]
-    },
     entry_points={
         "console_scripts": [
             "gotoball=gotoball:gotoball",
         ],
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
 )
